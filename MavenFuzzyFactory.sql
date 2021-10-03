bc935b0467c50fdb84be16959d2aa15771ea2a13
Jude Al-Attraqchi <jude.alattraqchi@gmail.com>
2021-10-03 21:36:48 +1300

    Update MavenFuzzyFactory.sql
    new update

--- MavenFuzzyFactory.sql
+++ MavenFuzzyFactory.sql
@@ -1,8 +1,12 @@
+/* Maven Fuzzy Factory is online store saling four items, I used open sourse data to create multi sinarios 
+
+*/
+
+
 use mavenfuzzyfactory;
 
-
 /*
-1.	Gsearch seems to be the biggest driver of our business. Could you pull monthly 
+1st sinario:	Gsearch seems to be the biggest driver of our business. Could you pull monthly 
 trends for gsearch sessions and orders so that we can showcase the growth there? 
 */ 
 
@@ -20,7 +24,7 @@ WHERE website_sessions.created_at < '2012-11-27'
 GROUP BY 1,2;
 
 /*
-2.	Next, it would be great to see a similar monthly trend for Gsearch, but this time splitting out nonbrand 
+2nd sinario.	Next, it would be great to see a similar monthly trend for Gsearch, but this time splitting out nonbrand 
 and brand campaigns separately. I am wondering if brand is picking up at all. If so, this is a good story to tell. 
 */ 
 
@@ -40,7 +44,7 @@ GROUP BY 1,2;
 
 
 /*
-3.	While we’re on Gsearch, could you dive into nonbrand, and pull monthly sessions and orders split by device type? 
+3rd sinario:	While we’re on Gsearch, could you dive into nonbrand, and pull monthly sessions and orders split by device type? 
 I want to flex our analytical muscles a little and show the board we really know our traffic sources. 
 */ 
 
@@ -62,7 +66,7 @@ GROUP BY 1,2;
 
 
 /*
-4.	I’m worried that one of our more pessimistic board members may be concerned about the large % of traffic from Gsearch. 
+4th sinario:	I’m worried that one of our more pessimistic board members may be concerned about the large % of traffic from Gsearch. 
 Can you pull monthly trends for Gsearch, alongside monthly trends for each of our other channels?
 */ 
 
@@ -91,7 +95,7 @@ GROUP BY 1,2;
 
 
 /*
-5.	I’d like to tell the story of our website performance improvements over the course of the first 8 months. 
+5thsinario:	I’d like to tell the story of our website performance improvements over the course of the first 8 months. 
 Could you pull session to order conversion rates, by month? 
 
 */ 
@@ -109,7 +113,7 @@ WHERE website_sessions.created_at < '2012-11-27'
 GROUP BY 1,2;
 
 /*
-6.	For the gsearch lander test, please estimate the revenue that test earned us 
+6th sinario:	For the gsearch lander test, please estimate the revenue that test earned us 
 (Hint: Look at the increase in CVR from the test (Jun 19 – Jul 28), and use 
 nonbrand sessions and revenue since then to calculate incremental value)
 */ 
@@ -207,7 +211,7 @@ WHERE created_at < '2012-11-27'
     
 
 /*
-7.	For the landing page test you analyzed previously, it would be great to show a full conversion funnel 
+7th sinario:	For the landing page test you analyzed previously, it would be great to show a full conversion funnel 
 from each of the two pages to orders. You can use the same time period you analyzed last time (Jun 19 – Jul 28).
 */ 
 
